e402698f636d8f0d05552e78f153569118820410
Meharab-27 <meharabrahman100115@gmail.com>
2025-10-18 11:54:29 +0600

    load news data

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { useParams } from 'react-router';
+
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
    
     const {id} = useParams();
-    console.log(id);
+    const data = useLoaderData();
+    const [categoryNews,setCategoryNews] = useState([]);
+
+   
+    useEffect(()=>{
+        if(id == "0"){
+            setCategoryNews(data);
+            return;
+
+        }else if(id == "1"){
+           const filterNews = data.filter(news =>news.others.is_today_pick == true );
+        setCategoryNews(filterNews);
+        }else{
+   const filterNews = data.filter(news =>news.category_id == id );
+        console.log(filterNews);
+        setCategoryNews(filterNews);
+        }
+        
+    },[data,id]);
     return (
         <div>
-            Category News - {id}
+            Total {categoryNews.length} found
         </div>
     );
 };
